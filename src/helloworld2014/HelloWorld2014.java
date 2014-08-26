204a4bb9caef572dc12e26bd699e6a0f3d7fa1d2
Len Payne <len.payne@lambtoncollege.ca>
2014-08-26 12:47:27 -0400

    Initial Commit

---
+++ src/helloworld2014/HelloWorld2014.java
@@ -0,0 +1,16 @@
+package helloworld2014;
+
+/**
+ * Hello World
+ * @author Len Payne
+ */
+public class HelloWorld2014 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
